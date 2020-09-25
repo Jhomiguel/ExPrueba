fa9e991440f13d1a8cda816f45a7b947df35370b
Jhomiguel <jhomiguelflorianmendez20@gmail.com>
2020-09-25 15:10:44 -0400

    Remove useEffect from UpdateEmployee

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
-require("dotenv").config({ path: "variables.env.local" });
+require("dotenv").config({ path: ".env.local" });
 
 const DBConnection = async () => {
-    try {
-        await mongoose.connect(process.env.DB_MONGO, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-        });
-        console.log("DB connected");
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.DB_MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log("DB connected");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
 
 module.exports = DBConnection;
